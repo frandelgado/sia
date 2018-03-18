2b94f4935f20664043bf67fbf1a56a7e4f6351b4
matuteale <mbuscagl@itba.edu.ar>
2018-03-18 14:38:03 -0300

    Add incremental and exponential function options

---
+++ incremental.m
@@ -0,0 +1,6 @@
+function ret = incremental(weightMatrices, number_of_cases)
+  global data;
+  for i = 1:number_of_cases
+      partial_weights = forward(weightMatrices, data(i, 1), data(i, 2));
+  endfor
+endfunction
--- main.m
+++ main.m
@@ -1,9 +1,20 @@
 # Prevent Octave from thinking that this is a function file:
 1;
-
 global n = 0.02;
+global activation_function = 1; # 0 for hyperbolic tangent or 1 for exponencial.
+global data = dlmread("terrain04.data"); # Reads data and stores it in a matrix.
+process_type = 0; # 0 for batch or 1 for incremental.
+number_of_cases = 50;
 architecture = [2, 3, 1]; # Each column specifies the amount of neurons in a layer.
 
 # Starting
-weightMatrices = init(architecture)
+weightMatrices = init(architecture);
 
+x = -10:0.1:10
+plot (x, sin (x))
+
+if process_type == 0
+  batch(weightMatrices, number_of_cases);
+else
+  incremental(weightMatrices, number_of_cases);
+endif
