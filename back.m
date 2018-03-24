b2f1d3111fadca64d160cc61796e30c0a113109e
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-03-24 19:00:00 -0300

    use linear output fn for last layer with exp fn

--- back.m
+++ back.m
@@ -1,9 +1,9 @@
 function [modified_weight_matrices, weight_matrices_diff] = back(weight_matrices, weight_matrices_diff, output_values, expected)
-  layers = length(weight_matrices) + 1;
+  layers = length(weight_matrices);
   external_layer = [output_values(end, 1)];
   init_deltas = g_prime(external_layer) * (expected - external_layer);
   deltas = [init_deltas];
-  for j = layers - 1:-1:1
+  for j = layers:-1:1
     [weight_matrices{j}, deltas, weight_matrices_diff{j}] = backpropagation(weight_matrices{j}, weight_matrices_diff{j}, j, deltas, output_values, layers);
   endfor
   modified_weight_matrices = weight_matrices;
@@ -13,6 +13,7 @@ function [modified_weights, deltas, new_weight_matrix_diff] = backpropagation(we
   global n;
   global momentum_alpha;
   global use_momentum;
+  global enable_linear_output
   layer_for_g_prime = output_values(index_to_use, 1:rows(weights_to_modify) - 1);
   layer_output = horzcat(layer_for_g_prime, -1);
   momentum = 0;
@@ -22,7 +23,11 @@ function [modified_weights, deltas, new_weight_matrix_diff] = backpropagation(we
   new_weight_matrix_diff = n*(layer_output' * deltas) + momentum;
   modified_weights = weights_to_modify + new_weight_matrix_diff;
   weights_for_new_deltas = modified_weights(1:end-1,:);
-  deltas = deltas * weights_for_new_deltas' .* g_prime(layer_for_g_prime);
+  if enable_linear_output == 1 && j == layers
+    deltas = deltas * weights_for_new_deltas';
+  else
+    deltas = deltas * weights_for_new_deltas' .* g_prime(layer_for_g_prime);
+  endif
 endfunction
 
 function ret = g_prime(layer)
--- main.m
+++ main.m
@@ -8,12 +8,13 @@ global a_for_adaptative_eta = 1.1;
 global b_for_adaptative_eta = 0.75;
 global steps_for_adaptative_eta = 5;
 global momentum_alpha = 0.9;
-global activation_function = 0; # 0 for hyperbolic tangent or 1 for exponencial.
+global activation_function = 1; # 0 for hyperbolic tangent or 1 for exponencial.
 global data = dlmread("terrain04.data"); # Reads data and stores it in a matrix.
 global number_of_cases = floor(0.7*(rows(data)-1));
 architecture = [2, 50, 1]; # Each column specifies the amount of neurons in a layer.
 global max_layer_neurons = max(architecture);
 global is_incremental = 1;
+global enable_linear_output = 1;
 
 # Activation function.
 function ret = sig_exp(z)
