74b00e3038ab52c0bc1d248fae97d19d2c9ed0b2
fdelgado <fdelgado@itba.edu.ar>
2018-03-24 01:47:30 -0300

    started adaptative eta

--- incremental.m
+++ incremental.m
@@ -1,24 +1,52 @@
 function ret = incremental(weight_matrices, number_of_cases)
   global data;
+  global n;
+  global a_for_adaptative_eta;
+  global b_for_adaptative_eta;
+  global steps_for_adaptative_eta;
+  global use_adaptative_eta;
+
   epoch = rows(data)-1;
   current_epoch_pattern = 1;
   min_error = 0.00001;
   epoch_error = 1;
+  prev_epoch_error = 1;
+  epoch_error_decreasing_steps = 0;
+  previous_weights = {};
   t = 1;
   error_time_matrix = ones(1, 3);
   while(epoch_error > min_error)
+    if use_adaptative_eta  
+      previous_weights = weight_matrices;
+    endif
+    
     while(current_epoch_pattern <= number_of_cases)
       pattern = randi([2, number_of_cases + 1]);
       output_values = forward(weight_matrices, data(pattern, 1), data(pattern, 2));
       weight_matrices = back(weight_matrices, output_values, [data(pattern, 3)]);
       current_epoch_pattern = current_epoch_pattern + 1;
     endwhile
+    prev_epoch_error = epoch_error;
+  
+    if use_adaptative_eta
+      if epoch_error < prev_epoch_error
+        epoch_error_decreasing_steps = epoch_error_decreasing_steps + 1;
+        if steps_for_adaptative_eta <= epoch_error_decreasing_steps
+          n = a_for_adaptative_eta + n;
+        endif
+      else
+        weight_matrices = previous_weights;
+        n = n - b_for_adaptative_eta*n;
+        epoch_error = prev_epoch_error;
+      endif
+    endif
+
     [epoch_error, error_time_matrix] = aproximation_error(number_of_cases, weight_matrices, data, error_time_matrix, t);
     error_time_matrix = generalization_error(number_of_cases, weight_matrices, data, error_time_matrix, t, epoch);
-    current_epoch_pattern = 0;
+    current_epoch_pattern = 1;
 
     plot(error_time_matrix(:,2:3));
-    axis([0, 1, 0, 0.1]);
+    axis([0, 1, 0, 0.5]);
     axis("autox");
     drawnow();
     t = t + 1;
--- main.m
+++ main.m
@@ -2,6 +2,10 @@
 1;
 global n = 0.02;
 global use_momentum = true;
+global use_adaptative_eta = true;
+global a_for_adaptative_eta = 0.01;
+global b_for adaptative_eta = 0.2;
+global steps_for_adaptative_eta = 3;
 global momentum_alpha = 0.9;
 global activation_function = 0; # 0 for hyperbolic tangent or 1 for exponencial.
 global data = dlmread("terrain04.data"); # Reads data and stores it in a matrix.
@@ -9,12 +13,11 @@ global number_of_cases = 300;
 architecture = [2, 50, 1]; # Each column specifies the amount of neurons in a layer.
 global max_layer_neurons = max(architecture);
 
-# Activation function
+# Activation function.
 function ret = sig_exp(z)
   ret = 1 ./ (1 + e.^-z);
 endfunction
 
-# Starting
+# Starting.
 weight_matrices = init(architecture);
-
-incremental(weight_matrices, number_of_cases);
+incremental(weight_matrices,number_of_cases);
