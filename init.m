1865b945c8be3bcf71b7f27b7afb4a69e4395291
matuteale <mbuscagl@itba.edu.ar>
2018-03-25 18:57:47 -0300

    Fix initial weights

--- init.m
+++ init.m
@@ -1,6 +1,7 @@
 function weight_matrices = init (architecture)
     arch_size = length(architecture);
     for i = 1:(arch_size-1)
-        weight_matrices{i} = -sqrt(3) + (2*sqrt(3) * rand(architecture(i) + 1, architecture(i+1))); # rows = from ; cols = to
+        adj = sqrt(6/(architecture(i) + 1 + architecture(i + 1)));
+        weight_matrices{i} = -adj + (2 * adj * rand(architecture(i) + 1, architecture(i+1))); # rows = from ; cols = to
     endfor
 endfunction
