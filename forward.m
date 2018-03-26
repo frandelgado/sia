b5ff14f99763eb68eeda1aa9e2586d285b91db23
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-03-26 11:50:44 -0300

    ups

--- forward.m
+++ forward.m
@@ -19,13 +19,13 @@ function ret = propagate(weight_matrix, output_values, current_input_index, last
     weight_matrix_size = rows(weight_matrix);
     current_inputs = output_values(current_input_index, 1:weight_matrix_size);
     current_inputs(weight_matrix_size) = -1;
-    if enable_linear_output == 1 && last_layer == current_input_index
-        with_applied_function = current_inputs * weight_matrix;
-    endif
     if activation_function == 0
         with_applied_function = tanh(current_inputs * weight_matrix);
     elseif activation_function == 1
         with_applied_function = sig_exp(current_inputs * weight_matrix);
+    endif
+    if enable_linear_output == 1 && last_layer == current_input_index
+        with_applied_function = current_inputs * weight_matrix;
     endif
 
     diff_size = max_layer_neurons + 1 - length(with_applied_function);
