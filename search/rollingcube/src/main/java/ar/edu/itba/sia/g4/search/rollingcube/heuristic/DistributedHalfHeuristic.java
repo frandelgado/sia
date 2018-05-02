24c3b04f47d667ba90b24d28292f0563001062b2
Lautaro Gonzalez <lautaro.ngc@gmail.com>
2018-05-01 22:05:08 -0300

    Add heuristics

---
+++ search/rollingcube/src/main/java/ar/edu/itba/sia/g4/search/rollingcube/heuristic/DistributedHalfHeuristic.java
@@ -0,0 +1,46 @@
+package ar.edu.itba.sia.g4.search.rollingcube.heuristic;
+
+import ar.com.itba.sia.Heuristic;
+import ar.edu.itba.sia.g4.search.rollingcube.action.FaceColor;
+import ar.edu.itba.sia.g4.search.rollingcube.game.Board;
+import ar.edu.itba.sia.g4.search.rollingcube.game.Cube;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DistributedHalfHeuristic implements Heuristic<Board> {
+    @Override
+    public double getValue(Board board) {
+        Map<FaceColor,Integer> colors = new HashMap<>();
+        double result = 0.0;
+        Cube cube;
+        FaceColor faceColor;
+        int i;
+        int j;
+        for(i = 0;i < 3; i++){
+            for(j = 0; j < 3;j++){
+                cube = board.getMatrix()[i][j];
+                faceColor = cube.getFaceColor();
+                if(board.getMatrix()[i][j] != null){
+                    switch (faceColor){
+                        case ALL_WHITE:
+                            result += 2.0;
+                        case ALL_BLACK:
+                            result += 3.0;
+                        default:
+                            Integer count = 0;
+                            if (colors.containsKey(faceColor)){
+                                count = colors.get(faceColor);
+                            }
+                            colors.put(faceColor, ++count);
+                            result += 1.0;
+                    }
+                }
+            }
+        }
+        for (Integer v: colors.values()){
+            result = result + Math.pow(v,1.5);
+        }
+        return result;
+    }
+}
--- search/rollingcube/src/main/java/ar/edu/itba/sia/g4/search/rollingcube/heuristic/MaxHalfHeuristic.java
+++ search/rollingcube/src/main/java/ar/edu/itba/sia/g4/search/rollingcube/heuristic/MaxHalfHeuristic.java
@@ -1,11 +1,10 @@
 package ar.edu.itba.sia.g4.search.rollingcube.heuristic;
 
 import ar.com.itba.sia.Heuristic;
-import ar.edu.itba.sia.g4.search.rollingcube.action.FaceColor;
 import ar.edu.itba.sia.g4.search.rollingcube.game.Board;
 import ar.edu.itba.sia.g4.search.rollingcube.game.Cube;
 
-public class MaxWhiteHeuristic implements Heuristic<Board> {
+public class MaxHalfHeuristic implements Heuristic<Board> {
     @Override
     public double getValue(Board board) {
         double result = 0.0;
@@ -15,11 +14,14 @@ public class MaxWhiteHeuristic implements Heuristic<Board> {
         for(i = 0;i < 3; i++){
             for(j = 0; j < 3;j++){
                 cube = board.getMatrix()[i][j];
-                if(board.getMatrix()[i][j] != null && cube.getFaceColor() != FaceColor.ALL_WHITE){
-                    if(cube.getFaceColor() == FaceColor.ALL_BLACK){
-                        result += 2.0;
-                    }else{
-                        result += 1.0;
+                if(board.getMatrix()[i][j] != null){
+                    switch (cube.getFaceColor()){
+                        case ALL_WHITE:
+                            result += 2.0;
+                        case ALL_BLACK:
+                            result += 3.0;
+                        default:
+                            result += 1.0;
                     }
                 }
             }
@@ -27,4 +29,3 @@ public class MaxWhiteHeuristic implements Heuristic<Board> {
         return result;
     }
 }
-
