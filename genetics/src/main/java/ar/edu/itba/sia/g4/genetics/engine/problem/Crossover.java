050d56da9c2578cac26ec2728f56cf4f424daa1c
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-05-20 18:58:09 -0300

    style(genetics): add documentation for inclusive language

--- genetics/src/main/java/ar/edu/itba/sia/g4/genetics/engine/problem/Crossover.java
+++ genetics/src/main/java/ar/edu/itba/sia/g4/genetics/engine/problem/Crossover.java
@@ -1,5 +1,11 @@
 package ar.edu.itba.sia.g4.genetics.engine.problem;
 
 public interface Crossover<T extends Species> {
+    /**
+     * Lenguaje inclusive
+     * @param mem
+     * @param ded
+     * @return
+     */
     T[] breed(T mem, T ded);
 }
