adeb1b4964f770169448ae0cdc94755fe93e31a7
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-05-20 18:58:38 -0300

    feat(genetics): wire up the first version of the project with a sample run

--- genetics/src/main/java/ar/edu/itba/sia/g4/genetics/cli/Main.java
+++ genetics/src/main/java/ar/edu/itba/sia/g4/genetics/cli/Main.java
@@ -1,9 +1,21 @@
 package ar.edu.itba.sia.g4.genetics.cli;
 
+import ar.edu.itba.sia.g4.genetics.dnd.DNDCharacter;
+import ar.edu.itba.sia.g4.genetics.dnd.Warrior1DNDCharacterSoup;
+import ar.edu.itba.sia.g4.genetics.dnd.crossers.NilCrosser;
+import ar.edu.itba.sia.g4.genetics.dnd.mutators.NilMutator;
+import ar.edu.itba.sia.g4.genetics.dnd.selectors.NilSelector;
+import ar.edu.itba.sia.g4.genetics.dnd.targets.NilTarget;
 import ar.edu.itba.sia.g4.genetics.engine.Darwin;
-import javafx.scene.web.HTMLEditorSkin;
+import ar.edu.itba.sia.g4.genetics.engine.problem.Crossover;
+import ar.edu.itba.sia.g4.genetics.engine.problem.EvolutionaryTarget;
+import ar.edu.itba.sia.g4.genetics.engine.problem.Mutator;
+import ar.edu.itba.sia.g4.genetics.engine.problem.PrimordialSoup;
+import ar.edu.itba.sia.g4.genetics.engine.problem.Selector;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
+
+import java.util.List;
 
 public class Main {
 
@@ -25,6 +37,17 @@ public class Main {
 
     public static void main(String... args) {
         CommandLineOptions options = parseArguments(args);
-        // Darwin charles = new Darwin();
+        PrimordialSoup<DNDCharacter> genesisPool = new Warrior1DNDCharacterSoup(10);
+        List<DNDCharacter> population = genesisPool.miracleOfLife();
+
+        Mutator<DNDCharacter> nilMutator = new NilMutator();
+        Crossover<DNDCharacter> nilCrosser = new NilCrosser();
+        Selector<DNDCharacter> nilSelector = new NilSelector();
+        EvolutionaryTarget<DNDCharacter> nilTarget = new NilTarget();
+
+        Darwin<DNDCharacter> charles = new Darwin(nilMutator, nilCrosser, nilSelector, nilTarget);
+        List<DNDCharacter> evolved = charles.evolve(population);
+
+        evolved.forEach(p -> System.out.println(p));
     }
 }
