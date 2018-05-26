edeabe124ccdad14895d0bceaf52ec35912bcd02
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2018-05-26 20:16:26 -0300

    feat(genetics): add config schema

---
+++ genetics/src/main/java/ar/edu/itba/sia/g4/genetics/config/AppConfig.java
@@ -0,0 +1,50 @@
+package ar.edu.itba.sia.g4.genetics.config;
+
+import java.io.Serializable;
+
+public class AppConfig implements Serializable {
+
+    private double generationalGap;
+
+    private int populationSize;
+
+    private String replacementAlgorithm;
+
+    private Items items;
+
+    public double getGenerationalGap() {
+        return generationalGap;
+    }
+
+    public AppConfig setGenerationalGap(double generationalGap) {
+        this.generationalGap = generationalGap;
+        return this;
+    }
+
+    public int getPopulationSize() {
+        return populationSize;
+    }
+
+    public AppConfig setPopulationSize(int populationSize) {
+        this.populationSize = populationSize;
+        return this;
+    }
+
+    public String getReplacementAlgorithm() {
+        return replacementAlgorithm;
+    }
+
+    public AppConfig setReplacementAlgorithm(String replacementAlgorithm) {
+        this.replacementAlgorithm = replacementAlgorithm;
+        return this;
+    }
+
+    public Items getItems() {
+        return items;
+    }
+
+    public AppConfig setItems(Items items) {
+        this.items = items;
+        return this;
+    }
+}
---
+++ genetics/src/main/java/ar/edu/itba/sia/g4/genetics/config/Items.java
@@ -0,0 +1,54 @@
+package ar.edu.itba.sia.g4.genetics.config;
+
+public class Items {
+    private String gauntlets;
+    private String helmets;
+    private String boots;
+    private String weapons;
+    private String chestplates;
+
+    public String getGauntlets() {
+        return gauntlets;
+    }
+
+    public Items setGauntlets(String gauntlets) {
+        this.gauntlets = gauntlets;
+        return this;
+    }
+
+    public String getHelmets() {
+        return helmets;
+    }
+
+    public Items setHelmets(String helmets) {
+        this.helmets = helmets;
+        return this;
+    }
+
+    public String getBoots() {
+        return boots;
+    }
+
+    public Items setBoots(String boots) {
+        this.boots = boots;
+        return this;
+    }
+
+    public String getWeapons() {
+        return weapons;
+    }
+
+    public Items setWeapons(String weapons) {
+        this.weapons = weapons;
+        return this;
+    }
+
+    public String getChestplates() {
+        return chestplates;
+    }
+
+    public Items setChestplates(String chestplates) {
+        this.chestplates = chestplates;
+        return this;
+    }
+}
