0225e8064d150498bce9afe347f442401fc6ceb3
matuteale <mbuscagl@itba.edu.ar>
2018-05-28 12:35:57 -0300

    Add Structure target

--- genetics/src/main/java/ar/edu/itba/sia/g4/genetics/dnd/targets/StructureTarget.java
+++ genetics/src/main/java/ar/edu/itba/sia/g4/genetics/dnd/targets/StructureTarget.java
@@ -2,33 +2,48 @@ package ar.edu.itba.sia.g4.genetics.dnd.targets;
 
 import ar.edu.itba.sia.g4.genetics.dnd.DNDCharacter;
 import ar.edu.itba.sia.g4.genetics.problem.EvolutionaryTarget;
+import ar.edu.itba.sia.g4.genetics.problem.Species;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class StructureTarget<T> implements EvolutionaryTarget<DNDCharacter> {
+public class StructureTarget<T extends Species> implements EvolutionaryTarget<T> {
 
-    List<DNDCharacter> previousPopulation;
-    List<DNDCharacter> currentPopulation;
+    List<T> previousPopulation;
+    List<T> currentPopulation;
+    Comparator<T> comparator;
 
-    public StructureTarget(List<DNDCharacter> previousPopulation, List<DNDCharacter> currentPopulation) {
+    public StructureTarget(List<T> previousPopulation, List<T> currentPopulation) {
         this.previousPopulation = previousPopulation;
         this.currentPopulation = currentPopulation;
+        this.comparator = new SortComparator();
     }
 
-    private boolean equal(DNDCharacter c1, DNDCharacter c2){
-        return c1.getFitness() - c2.getFitness() < 0.00001;
-
-
+    private boolean equal(T c1, T c2){
+        return Math.abs(c1.getFitness() - c2.getFitness()) < 0.00001;
     }
 
     @Override
-    public boolean shouldEvolve(List<DNDCharacter> prev, List<DNDCharacter> curr, long generation) {
+    public boolean shouldEvolve(List<T> prev, List<T> curr, long generation) {
+        double equalAmount = curr.size() * 0.0002;
+        List<T> sortedPrev = prev.parallelStream().sorted(this.comparator.reversed()).collect(Collectors.toList());
+        List<T> sortedCurr = curr.parallelStream().sorted(this.comparator.reversed()).collect(Collectors.toList());
+        int n = IntStream.range(0,prev.size()).parallel()
+                .filter(i -> !equal(sortedCurr.get(i), sortedPrev.get(i)))
+                .findFirst().getAsInt();
+        return n <= 0 && (n - 1) <= equalAmount;
+    }
 
-//        int n = IntStream.range(0,prev.size()).parallel()
-//                .findFirst(i -> {
-//                    return equal(curr.get(i), prev.get(i));
-//                });
-        return true;
+    private class SortComparator<T extends Species> implements Comparator<T> {
+        @Override
+        public int compare(T o1, T o2) {
+            double res = o1.getFitness() - o2.getFitness();
+            if(res == 0.0){
+                return 0;
+            }
+            return ((o1.getFitness() - o2.getFitness()) > 0.0)?1:0;
+        }
     }
 }
