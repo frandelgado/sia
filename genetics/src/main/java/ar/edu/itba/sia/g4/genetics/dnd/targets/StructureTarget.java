7912644cf62c9067fb6de7af8999309d39bd679b
matuteale <mbuscagl@itba.edu.ar>
2018-05-30 02:47:53 -0300

    Modify structural implementation

--- genetics/src/main/java/ar/edu/itba/sia/g4/genetics/dnd/targets/StructureTarget.java
+++ genetics/src/main/java/ar/edu/itba/sia/g4/genetics/dnd/targets/StructureTarget.java
@@ -3,11 +3,8 @@ package ar.edu.itba.sia.g4.genetics.dnd.targets;
 import ar.edu.itba.sia.g4.genetics.problem.EvolutionaryTarget;
 import ar.edu.itba.sia.g4.genetics.problem.Species;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class StructureTarget<T extends Species> implements EvolutionaryTarget<T> {
     private final int iterations;
@@ -31,26 +28,20 @@ public class StructureTarget<T extends Species> implements EvolutionaryTarget<T>
         generations.add(curr);
         if (generations.size() <= iterations) {
             return true;
+        } else {
+            generations.remove(0);
         }
-        generations.remove(0);
 
-        List<List<T>> auxGenerations = generations.stream().map(gen ->
-            gen.parallelStream().map(item -> ((T)item.deepCopy())).collect(Collectors.toList())
-        ).collect(Collectors.toList());
+        int survivors = curr.stream()
+                .distinct()
+                .parallel()
+                .mapToInt(thing -> generations.stream()
+                        .parallel()
+                        .mapToInt(g -> (int) g.stream().filter(thing::equals).count())
+                        .min()
+                        .orElse(0))
+                .sum();
 
-        long sharedPopulation = curr.stream().filter((T currItem) -> {
-            if(auxGenerations.parallelStream()
-                .allMatch(gen -> gen.parallelStream()
-                        .anyMatch(item -> item.equals(currItem)))){
-            auxGenerations.parallelStream().forEach(gen -> gen.remove(currItem));
-            return true;
-        }
-        return false;
-        }).count();
-        System.out.println("gen: " + generation);
-        System.out.println("shared: " + sharedPopulation);
-        System.out.println("delta: " + delta * curr.size());
-        return sharedPopulation < delta * curr.size();
+        return survivors < delta * curr.size();
     }
-
 }
